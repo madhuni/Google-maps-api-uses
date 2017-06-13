a38018028474a13bdd3d6a6b7060a63955d788d5
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-06-13 10:59:35 +0530

    Added the functionality in the marker.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,38 @@
 var map;
 var marker;
 function initMap () {
-    console.log("I am working");
     map = new google.maps.Map(document.getElementById("my-map"), {
-        center: {lat: 28.845614, lng: 78.767456},
-        zoom: 15
+        center: {lat: 40.7413549, lng: -73.9980244},
+        zoom: 13,
+        mapTypeId: 'setallite'
     });
     
+    var markerPosition = {lat: 40.719526, lng: -74.0089934};
+    
+    /* Creating the marker by passing the options in the constructor itself */
     marker = new google.maps.Marker({
-        position: {lat: 28.845614, lng: 78.767456},
+        position: markerPosition,
         map: map,
-        title: 'First Marker'
+        title: "Hello World !!!",
+        draggable: false,
+        animation: google.maps.Animation.DROP
     });
+    
+    marker.addListener('click', toggleBounce);
+    
+    /* Passing the empty Object in the Marker constructor */
+    // marker = new google.maps.Marker({});
+    
+    /* Setting the Options value separately by using 'set' methods of Marker Object */
+    // marker.setPosition(markerPosition);
+    // marker.setMap(map);
+    // marker.setTitle("Hello World !!!");
+};
+
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
 };
